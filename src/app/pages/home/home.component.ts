9fa81d28866324043a35dfbd226fb0813a9724b7
RafaelP5 <rafaelpatricioayllon123@gmail.com>
2023-11-17 20:53:16 -0400

    Actualizar

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,70 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LanguagesService } from 'src/app/services/languages.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+  
+  name:string = "Python";
+  abrev:string = "Py";
+  dataSource:any = [];
 
+  constructor(private language: LanguagesService){}
+
+  ngOnInit()
+  {
+    this.language.getListLanguges().subscribe( (data) => {
+      for(var key in data)
+      {
+        var row = {id:key, abrev: data[key].abrev, name: data[key].name}
+        this.dataSource.push(row)
+      }
+      console.log(this.dataSource)
+    } )
+  }
+
+  save()
+  {
+    let body = 
+    {
+      name: this.name,
+      abrev: this.abrev
+    }
+    this.language.postLanguage(body).subscribe( (data) => {
+      if(data!=null)
+      {
+        window.location.reload();
+      }
+    })
+  }
+
+  borrar(id:string){
+    let aux = confirm("Esta Seguro de Borrar")
+    if(!aux) return
+    this.language.deleteLanguage(id).subscribe( (data) => {
+      if(data==null)
+      {
+        window.location.reload();
+      }
+    })
+  }
+
+  actualizar(id:string){
+    let aux = confirm("Esta Seguro de Actualizar")
+    let body = 
+    {
+      abrev: "test Upt abrev",
+      name:  "test Upt name"
+    }    
+    if(!aux) return
+    this.language.updateLanguage(id, body).subscribe( (data) => {
+      if(data!=null)
+      {
+        window.location.reload();
+      }
+    })
+  }
 }
